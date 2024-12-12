3f2d07c9e45d2ee02a5c83804a68a0f25a51a403
Hao Zhang <hzhangxyz@outlook.com>
2024-12-13 02:35:05 +0800

    添加非正交命题逻辑。

--- grammar/v1.rkt
+++ grammar/v1.rkt
@@ -4,17 +4,12 @@
 
 (provide (except-out (all-from-out racket) #%module-begin)
          (rename-out [module-begin #%module-begin])
-         恣
-         基)
-
-(define (基 . 文件)
-  (map (lambda (文件) (dynamic-require 文件 '数据)) 文件))
+         恣)
 
 (define-syntax-rule (module-begin 导入 表达式 ...)
-  (#%module-begin (define 导入数据 导入)
+  (#%module-begin (define-values (目录名 文件名 _)
+                    (split-path (variable-reference->module-source (#%variable-reference))))
+                  (define 导入数据 (map (lambda (文件) (dynamic-require (build-path 目录名 文件) '数据)) 导入))
                   (define 局部数据 (list 表达式 ...))
                   (define 数据 (remove-duplicates (append (append* 导入数据) 局部数据)))
-                  (displayln "")
-                  (for ([事实 数据])
-                    (displayln (展示 事实)))
                   (provide 数据)))
--- test/prop-small.rkt
+++ test/prop-small.rkt
@@ -2,13 +2,13 @@
 
 (require (file "../engine.rkt"))
 
-(define 命题逻辑 (dynamic-require "../database/prop.rkt" '数据))
+(define 命题逻辑 (dynamic-require "../database/prop-small.rkt" '数据))
 
 (define 这个世界 (append 命题逻辑 (list (恣 (非 (非 X))))))
 
 (define 多演结果
   (多演 这个世界
-      2
+      8
       (foldr max 0 (map (lambda (事实) (长度 (cdr 事实))) 这个世界))
       (lambda (x)
         (displayln "")
